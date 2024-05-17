67fd8bf033f97fbc8008dd515d9ff615dca51538
Kristýna Konečná <kristyna.konecna02@gmail.com>
2024-05-17 16:21:51 +0200

    Malá oprava funkce

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 const shortenText = () => {
   const cardTexts = document.querySelectorAll('[data-max-words]');
 
-  cardTexts.forEach(cardText => {
-    const maxWords = parseInt(cardText.getAttribute("data-max-words"), 13);
-    const originalText = cardText.getAttribute("data-original-text") || cardText.textContent.trim();
+  cardTexts.forEach((cardText) => {
+    const maxWords = parseInt(cardText.getAttribute('data-max-words'));
+    const originalText =
+      cardText.getAttribute('data-original-text') ||
+      cardText.textContent.trim();
 
-    if (!cardText.getAttribute("data-original-text")) {
-      cardText.setAttribute("data-original-text", originalText);
+    if (!cardText.getAttribute('data-original-text')) {
+      cardText.setAttribute('data-original-text', originalText);
     }
 
-    if (window.matchMedia("(max-width: 350px)").matches) {
+    if (window.matchMedia('(max-width: 350px)').matches) {
       const wordsArray = originalText.split(/\s+/);
       if (wordsArray.length > maxWords) {
-        const updatedText = wordsArray.slice(0, maxWords).join(" ") + "...";
+        const updatedText = wordsArray.slice(0, maxWords).join(' ') + '...';
         cardText.textContent = updatedText;
       } else {
         cardText.textContent = originalText;
@@ -23,5 +25,5 @@ const shortenText = () => {
   });
 };
 
-window.addEventListener("load", shortenText);
-window.addEventListener("resize", shortenText);
+window.addEventListener('load', shortenText);
+window.addEventListener('resize', shortenText);
